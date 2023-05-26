b2070a35df6a0a34a1117beb16ce48b66479afa9
davamo <8826599-davamo@users.noreply.gitlab.com>
2023-05-26 10:56:15 -0400

    Fixed: seeds/ twets user_name, descriptions

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,14 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-50.times do
+50.times do |i|
     description = Faker::Lorem.sentence
-    puts description
+    user_name = Faker::Internet.user_name
+  
+    tweet = Tweet.create(
+      description: description,
+      user_name: user_name
+    )
+  
+    puts tweet.inspect
   end
